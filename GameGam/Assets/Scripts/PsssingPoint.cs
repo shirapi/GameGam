ee41efcb9b413b897daadb80ee5bc8c7e636394d
DESKTOP-GQ4UTCI\池尻泰基 <taiki1222sp@gmail.com>
2018-10-14 19:45:10 +0900

    Titleシーン作成 BGMの追加

---
+++ GameGam/Assets/Scripts/PsssingPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsssingPoint : Trigger2 {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void Action()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(true);
+        Destroy(this.transform.GetChild(1).gameObject);
+        //this.transform.GetChild(1).gameObject.SetActive(false);
+        //Utility.DelayMethod(1.0f, () => Destroy(this.gameObject));
+    }
+}
---
+++ GameGam/Assets/Scripts/Trigger2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger2 : MonoBehaviour {
+
+    [SerializeField] private GameObject[] targets;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (targets == null) return;
+        foreach (GameObject tgt in targets)
+        {
+            if (other.gameObject == tgt) this.Action();
+        }
+    }
+
+    protected virtual void Action() { }
+}
