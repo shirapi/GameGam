0795a2d29f2ac21626bdccf639c904b3eb4cf697
shirapi <yusei29game@gmail.com>
2018-10-26 16:54:39 +0900

    シーン遷移 サイズ調整

--- GameGam/Assets/Scripts/Utility.cs
+++ GameGam/Assets/Scripts/Utility.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 class Utility
 {
+    //外記のように呼び出してください
+    //StartCoroutine(DelayMethod(1.0f, ()=>{ }));
     public static IEnumerator DelayMethod(float waitTime, Action action)
     {
         yield return new WaitForSeconds(waitTime);
